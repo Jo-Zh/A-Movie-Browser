9c77de821abef9ebf0d8e715eb060f815ec182c2
JoannaWeinaZhong <93627519+JoannaWeinaZhong@users.noreply.github.com>
2021-11-28 20:33:39 +1000

    Update App.js
    Add Not Found Page

--- App.js
+++ App.js
@@ -55,8 +55,8 @@ function App() {
             )
           }
         />
-
         <Route path="movies/:id" element={<MovieView />} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </div>
   );
